cbe9995537d27bee2fbb5cf8bebe5222a9d3f4f4
mikakallberg <mikaela.kallberg@hotmail.com>
2022-05-27 07:13:43 +0000

    This is a funny commit

--- assets/js/game.js
+++ assets/js/game.js
@@ -54,4 +54,13 @@ function showScore() {
     document.getElementById("score").innerText = game.score;
 }
 
-module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns };
+function playerTurn() {
+    let i = game.playerMoves.lenth - 1;
+    if(game.currentGame[i] === game.playerMoves[i]){
+        game.score++;
+        showScore();
+        addTurn();
+    }
+}
+
+module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns, playerTurn };
